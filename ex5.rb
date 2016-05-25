519d953c76c567439dae40650f1bb2c92e9d9cf3
Morgan Burrows <burrowmt@gmail.com>
2016-05-25 02:30:57 -0600

    Exercise 5: More Variables and Printing

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+name = 'Morgan T. Burrows'
+age = 20 # as of May 25th 2016
+height = 76.0 # inches (6ft 4in)
+weight = 200.0 # lbs
+eyes = 'grey'
+teeth = 'white'
+hair = 'brown'
+
+puts "Lets talk about #{name}."
+puts "He's #{height} inches and #{height * 2.54} centimeters tall."
+puts "He's #{weight} pounds and #{weight * 0.453592} kilograms heavy."
+puts "Thats pretty heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are #{teeth} and straight."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
