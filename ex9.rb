300b9d204871650f6fa37d9ea2969527d8eaf47a
Morgan Burrows <burrowmt@gmail.com>
2016-05-26 04:01:45 -0600

    Exercise 9: Printing, Printing, Printing

---
+++ ex9.rb
@@ -0,0 +1,15 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+theres something going on here.
+with the three double quotes.
+we'll be able to type as much as we like.
+even 4 lines if we want, or 5 or 6.
+"""
+
+# that isnt really that ground breaking. it just keeps registering input as a string until the "" is closed
+# evidently, \n is a newline char
