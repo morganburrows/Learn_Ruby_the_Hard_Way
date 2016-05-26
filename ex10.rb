6e8a0d76e181a4a513a87c9d7cfdf65908238764
Morgan Burrows <burrowmt@gmail.com>
2016-05-26 04:02:25 -0600

    Exercise 10: What Was That?

---
+++ ex10.rb
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in." # it has tab in the joke so im assuming \t is tab
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
