a65fbbdbf648029a256aa50ba7e15e40cac01267
Morgan Burrows <burrowmt@gmail.com>
2016-05-28 16:45:28 -0600

    Exercise 13: Parameters, Unpacking, Variables

---
+++ ex13.rb
@@ -0,0 +1,10 @@
+first, second, third = ARGV
+
+puts "your first variable is: #{first}"
+puts "your second variable is: #{second}"
+puts "your third variable is: #{third}"
+
+puts "enter a fourth variable."
+fourth = gets.chomp
+
+puts "your fourth variable is #{fourth}."
